7e01204597ead1373ed4c0e91865d6cf9607ae0f
rashmi.o <rashmi.o@WSLKCMP2F-09.SLKSOFT.COM>
2019-04-17 12:08:40 +0530

    Signed-off-by: rashmi.o <rashmi.o@WSLKCMP2F-09.SLKSOFT.COM>

---
+++ src/main/java/com/slk/SOAPWebService/SLK_POCProject/SlkPocProjectApplication.java
@@ -0,0 +1,13 @@
+package com.slk.SOAPWebService.SLK_POCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlkPocProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlkPocProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slk/SOAPWebService/SLK_POCProject/SlkPocProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.slk.SOAPWebService.SLK_POCProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlkPocProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
